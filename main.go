945de8e5a6b68a817baee3e8460bd83baf946f91
Thiago Kasper de Souza <thiagokaspersouza@gmail.com>
2024-06-04 04:02:24 -0300

    feat: Implements Server

--- main.go
+++ main.go
@@ -2,6 +2,8 @@ package main
 
 import (
 	"encoding/json"
+	"fmt"
+	"net/http"
 	r "newsRestFiber/src/repository"
 	m "newsRestFiber/src/repository/models"
 
@@ -27,19 +29,52 @@ You should have received a copy of the GNU General Public License
 along with rsNews_blogApi.  If not, see <https://www.gnu.org/licenses/>
 */
 
+const (
+	GET  = "GET "
+	POST = "POST "
+)
+
 func main() {
 	rdb := r.DbClient{
 		Instance: r.GetClient(),
 	}
-	d := m.Donation{
-		Id:   uuid.New().String(),
-		Nome: "TESTE",
-		Link: "link1",
+
+	router := http.NewServeMux()
+	router.HandleFunc(POST+"/", func(w http.ResponseWriter, r *http.Request) {
+		r.Header.Set("Content-Type", "application/json")
+
+		fmt.Println(r.Body)
+		d := m.Donation{
+			Id:   uuid.New().String(),
+			Nome: "teste",
+			Link: "link1",
+		}
+		res, err := json.Marshal(d)
+		if err != nil {
+			panic(err)
+		}
+		d.Create(rdb, "donations", d.Id, res)
+
+		json.NewEncoder(w).Encode(d)
+	})
+
+	server := http.Server{
+		Addr:    ":8080",
+		Handler: router,
 	}
-	res, err := json.Marshal(d)
-	if err != nil {
-		panic(err)
-	}
-	d.Create(rdb, "donations", d.Id, res)
+
+	fmt.Println("Server listening on http://localhost:8080")
+	server.ListenAndServe()
+
+	// d := m.Donation{
+	// 	Id:   uuid.New().String(),
+	// 	Nome: "TESTE",
+	// 	Link: "link1",
+	// }
+	// res, err := json.Marshal(d)
+	// if err != nil {
+	// 	panic(err)
+	// }
+	// d.Create(rdb, "donations", d.Id, res)
 
 }
